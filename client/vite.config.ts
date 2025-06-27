2e75eea464596c07965cf404c82be3d7cb32e1a3
fulpopy <akash16799@gmail.com>
2025-06-27 16:43:52 +0530

    feat: add deploy script to package.json and set base path in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/DSPL-ORDERS/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
