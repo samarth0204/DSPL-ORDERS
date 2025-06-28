85444fa1392c2dd6e005639396a0e48e157ba369
fulpopy <akash16799@gmail.com>
2025-06-28 17:09:50 +0530

    chore: update Vite configuration for improved project structure

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/DSPL-ORDERS/",
+  base: "/DSPL-ORDERS/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
