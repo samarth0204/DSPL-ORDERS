638004b3ae63e81f72a3b2df2fec7ef3f602a199
Akash Deep Singh <akash16799@gmail.com>
2025-08-08 02:10:22 +0530

    fix cors issue

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import orderRoutes from "./routes/order.route";
 import fulfillmentRoutes from "./routes/fulfillment.route";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/orders", orderRoutes);
 app.use("/api/fulfillment", fulfillmentRoutes);
