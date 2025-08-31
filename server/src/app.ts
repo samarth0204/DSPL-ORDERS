52ddbe15ed76aaaa9d6d544a7f4042993d9e5a75
Samarth Saini <62097591+samarth0204@users.noreply.github.com>
2025-08-31 19:22:42 +0530

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,9 +15,9 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../../client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
 });
 
 app.use("/api/orders", orderRoutes);
