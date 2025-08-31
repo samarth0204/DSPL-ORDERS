f4d15207b40cf685910e4fcf5ebd599dace074bb
Akash Deep Singh <akash16799@gmail.com>
2025-08-31 20:20:05 +0530

    removed satic page

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,7 +4,6 @@ import fulfillmentRoutes from "./routes/fulfillment.route";
 import userRoutes from "./routes/user.route";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
 
 const app = express();
 app.use(express.json());
@@ -15,10 +14,6 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../../client/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
-});
 
 app.use("/api/orders", orderRoutes);
 app.use("/api/fulfillment", fulfillmentRoutes);
