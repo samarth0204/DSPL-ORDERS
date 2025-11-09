dd2869a8dd138818f24ebf5454110b381da66615
Akash Deep Singh <akash16799@gmail.com>
2025-11-09 21:14:05 +0530

    fix

--- client/src/components/pages/Reports.tsx
+++ client/src/components/pages/Reports.tsx
@@ -1,13 +1,10 @@
 "use client";
 
 import { useState } from "react";
-import { Plus, Sun, Moon, CalendarIcon } from "lucide-react";
+import { Plus } from "lucide-react";
 import { Button } from "../ui/button";
 import { DailyReportForm } from "../common/DailyReportForm";
 import { useFetchReports } from "@/hooks/reportHooks";
-import { Card, CardHeader, CardTitle, CardContent } from "../ui/card";
-import { Badge } from "../ui/badge";
-import { Separator } from "../ui/separator";
 import ReportCard from "../common/ReportCard";
 
 const Reports = () => {
