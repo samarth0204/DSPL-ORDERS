0dd9151c829cd871f81432d44fac621cb12ba617
Samarth Saini <62097591+samarth0204@users.noreply.github.com>
2025-12-06 16:13:24 +0530

    https

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://dspl-server-288823573837.asia-south2.run.app/api", GCP api end point
-  baseURL: "http://api.divydaminispices.com/api",
+  baseURL: "https://api.divydaminispices.com/api",
   // baseURL: "http://localhost:3001/api",
 });
 
